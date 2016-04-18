75f277651449e6e9e7404dd69874f3cf661c6019
Kimiko Kano <kskano@gmail.com>
2016-04-18 10:31:24 -0600

    final commit

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,8 @@
 class SearchController < ApplicationController
   def index
-
     apikey = ENV['NREL_KEY']
-    connection = Faraday.new(:url => "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{apikey}&location=80203&radius=6&fuel_type=ELEC,LPG&limit=10")
-
-
+    connection = Faraday.new(:url => "https://developer.nrel.gov")
+    response = connection.get("/api/alt-fuel-stations/v1/nearest.json?api_key=#{apikey}&location=80203&radius=6&fuel_type=ELEC,LPG&limit=10")
+    result = reponse.body
   end
 end
