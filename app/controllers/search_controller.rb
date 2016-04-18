b20150bf23de2b5890bc9cf76785423112e0f09c
Kimiko Kano <kskano@gmail.com>
2016-04-18 10:26:36 -0600

    Add Faraday route

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,8 @@ class SearchController < ApplicationController
   def index
 
     apikey = ENV['NREL_KEY']
-    binding.pry
-    connection = Faraday.new(:url => "https://developer.nrel.gov/api/alt-fuel-stations/v1")
-    response = connection.get ("/nearest.json?api_key=#{apikey}&location=80203&radius=6&fuel_type=ELEC,LPG&limit=10")
+    connection = Faraday.new(:url => "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{apikey}&location=80203&radius=6&fuel_type=ELEC,LPG&limit=10")
+
 
   end
 end
--- spec/user_can_find_stations_by_zip_spec.rb
+++ spec/user_can_find_stations_by_zip_spec.rb
@@ -3,10 +3,10 @@ require "rails_helper"
 RSpec.feature "User can find stations by zip code" do
   scenario "they see the 10 closest stations within 6 miles sorted by distance" do
     visit "/"
-    fill_in text_field_tag, with: "80203"
+    fill_in "Search", with: "80203"
     click_on "Locate"
     expect(current_path).to eq "/search?zip=80203"
-    expect(page).to have_content
+    # expect(page).to have_content
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     # And the stations should be limited to Electric and Propane
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times# Then I should be on page "/search?zip=80203"
